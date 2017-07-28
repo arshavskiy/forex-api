b8b826ef24c5a6b8ba491f87b36f9831f27c186a
arshavskiy <pasha.websbee@gmail.com>
2017-07-29 00:49:17 +0300

    SEF added

--- js/main.js
+++ js/main.js
@@ -1,98 +1,97 @@
-function getForex(value, corrency) {
-    var url = "https://forex.1forge.com/1.0.1/convert";
-    url += '?' + $.param({
-        'from': correncyVld(corrency)[0],
-        'to': correncyVld(corrency)[1],
-        'quantity': value,
-        'api_key': 'bdaRvuzmDNy8pOUQjbiXz5GN87CkiSCY'
-    });
-    return url;
-}
+(function () {
 
-function correncyVld(val) {
-    return val.split("/");
-}
+    var init = (function () {
+        $('main').hide();
+        loader();
+        getCorency();
+    })();
 
-function getCorency() {
-
-    var url = 'https://forex.1forge.com/1.0.1/quotes?pairs=EURUSD,GBPJPY,AUDUSD&api_key=bdaRvuzmDNy8pOUQjbiXz5GN87CkiSCY';
-    $.ajax({
-        url: url,
-    }).done(function (data) {
-
-        done();
-
-        for (var i = 0; i < data.length; i++) {
-            buildTable(data[i].symbol, data[i]);
-        }
-    });
-};
-
-function done() {
-    $('#loader-container').hide();
-    $('main').show(300);
-}
-
-function getTime(time) {
-    var d = new Date(time * 1000);
-    return d.getHours() + '\:' + d.getMinutes() + '\:' + d.getSeconds();
-}
-
-function buildTable(tableName, d) {
-    var price = d.price.toFixed(3);
-
-    $(`#${tableName}`).text(d.symbol.slice(0, 3) + '/' + d.symbol.slice(3));
-    $(`#time-${tableName}`).text(getTime(d.timestamp));
-    $(`#price-${tableName}`).text(price).css("font-weight", "600");
-    $(`#symbol-${tableName}`).text(d.symbol);
-}
+    function loader() {
+        $('#loader-container', {
+            html: $('<div>', {
+                class: 'loader',
+                id: 'loader-2',
+                html: $('<span>'),
+            }).appendTo('#loader-container'),
+        }).prependTo('body');
+        $('<span>').appendTo('#loader-2');
+        $('<span>').appendTo('#loader-2');
+    };
 
 
-$('#form-submit').click(function (e) {
-    e.preventDefault();
-
-    var value = $('#f1').val();
-    var corrency = $('#f2').val();
-
-    if (value) {
-        $.get(getForex(value, corrency), function (data) {
-            if (data.error) {
-                showError();
-            }
-            $('#r1').val((data.value).toFixed(2));
+    function getForex(value, corrency) {
+        var url = "https://forex.1forge.com/1.0.1/convert";
+        url += '?' + $.param({
+            'from': correncyVld(corrency)[0],
+            'to': correncyVld(corrency)[1],
+            'quantity': value,
+            'api_key': 'bdaRvuzmDNy8pOUQjbiXz5GN87CkiSCY'
         });
-    } else showError();
-});
+        return url;
+    }
+    var correncyVld = (val) => val.split("/");
 
-function showError() {
-    $('#r1').val("Please enter the corrent value").fadeIn()
-    setTimeout(function () {
-        $('#r1').val("");
-    }, 2000);
 
-    // $('<strong>', {
-    //     text: "Please enter the corrent value",
-    // }).appendTo('#error').fadeIn().delay(2000).fadeOut();
-}
+    function getCorency() {
 
-$('#now-submit').click(function () {
-    $('.jumbotron').toggle(200);
-})
+        var url = 'https://forex.1forge.com/1.0.1/quotes?pairs=EURUSD,GBPJPY,AUDUSD&api_key=bdaRvuzmDNy8pOUQjbiXz5GN87CkiSCY';
+        $.ajax({
+            url: url,
+        }).done(function (data) {
 
-var init = (function () {
-    $('main').hide();
-    loader();
-    getCorency();
-})();
+            (function stopLoader() {
+                $('#loader-container').hide();
+                $('main').show(300);
+            }());
 
-function loader() {
-    $('#loader-container', {
-        html: $('<div>', {
-            class: 'loader',
-            id: 'loader-2',
-            html: $('<span>'),
-        }).appendTo('#loader-container'),
-    }).prependTo('body');
-    $('<span>').appendTo('#loader-2');
-    $('<span>').appendTo('#loader-2');
-};
+            for (var i = 0; i < data.length; i++) {
+                buildTable(data[i].symbol, data[i]);
+            }
+        });
+    };
+
+
+    function getTime(time) {
+        var d = new Date(time * 1000);
+        return d.getHours() + '\:' + d.getMinutes() + '\:' + d.getSeconds();
+    }
+
+    function buildTable(tableName, d) {
+        var price = d.price.toFixed(3);
+
+        $(`#${tableName}`).text(d.symbol.slice(0, 3) + '/' + d.symbol.slice(3));
+        $(`#time-${tableName}`).text(getTime(d.timestamp));
+        $(`#price-${tableName}`).text(price).css("font-weight", "600");
+        $(`#symbol-${tableName}`).text(d.symbol);
+    }
+
+
+    $('#form-submit').click(function (e) {
+        e.preventDefault();
+
+        var value = $('#f1').val();
+        var corrency = $('#f2').val();
+
+        if (value) {
+            $.get(getForex(value, corrency), function (data) {
+                if (data.error) {
+                    showError();
+                }
+                $('#r1').val((data.value).toFixed(2));
+            });
+        } else showError();
+    });
+
+    var showError = () => {
+        $('#r1').val("Please enter the corrent value").fadeIn()
+        setTimeout(function () {
+            $('#r1').val("");
+        }, 2000);
+    }
+
+    $('#now-submit').click(function () {
+        $('.jumbotron').toggle(200);
+    })
+
+
+}());
